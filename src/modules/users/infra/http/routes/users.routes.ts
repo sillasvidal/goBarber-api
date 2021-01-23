f35d6ea3aebc4ca0b13508108954084dc9c13630
Sillas Vidal <svidalempresa@gmail.com>
2021-01-23 10:08:26 -0300

    Second version of api GoBarber

---
+++ src/modules/users/infra/http/routes/users.routes.ts
@@ -0,0 +1,26 @@
+import { Router } from 'express';
+import { container } from 'tsyringe';
+import multer from 'multer';
+import uploadConfig from '@config/upload';
+
+import UsersController from '../controllers/UsersController';
+import UserAvatarController from '../controllers/UserAvatarController';
+
+
+import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
+
+const usersRouter = Router();
+const usersController = new UsersController();
+const userAvatarController = new UserAvatarController();
+const upload = multer(uploadConfig);
+
+usersRouter.post('/', usersController.create);
+
+usersRouter.patch(
+  '/avatar',
+  ensureAuthenticated,
+  upload.single('avatar'),
+  async (request, response) => userAvatarController.update
+);
+
+export default usersRouter;
